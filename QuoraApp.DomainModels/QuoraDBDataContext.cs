4cea675376b8ada6e33aa875a0794b3f5b57298f
Pritesh2305 <bhavsarpritesh.mca@gmail.com>
2020-08-14 09:55:54 +0530

    Set Project

---
+++ QuoraApp.DomainModels/QuoraDBDataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuoraApp.DomainModels
+{
+    public class QuoraDBDataContext :DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Question> Questions { get; set; }
+
+        public DbSet<Vote> Votes { get; set; }
+
+        public DbSet<Answer> Answers { get; set; }
+
+    }
+}
