a6d9ab82cb4e8ed1a08f1429ec77e9a7fe87d920
Pritesh2305 <bhavsarpritesh.mca@gmail.com>
2020-08-24 11:22:05 +0530

    Add Bundling

---
+++ QuoraApp.UI/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace QuoraApp.UI.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap.js").Include(
+                "~/Scripts/bootstrap.js,~/Scripts/jquery-{version}.js,~/Scripts/popper.js"));
+
+            bundles.Add(new StyleBundle("~/Scripts/bootstrap.css").Include(
+                "~/Content/bootstrap.css"));
+        }
+    }
+}
--- QuoraApp.UI/Global.asax.cs
+++ QuoraApp.UI/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using QuoraApp.UI.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace QuoraApp.UI
@@ -12,6 +14,7 @@ namespace QuoraApp.UI
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
